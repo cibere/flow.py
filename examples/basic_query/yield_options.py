5aaca55fecf72e2f987c6dac170ab417c28827ec
cibere <71997063+cibere@users.noreply.github.com>
2024-11-11 23:45:50 -0800

    Update yield_options.py

--- examples/basic_query/yield_options.py
+++ examples/basic_query/yield_options.py
@@ -1,10 +1,10 @@
 from flowpy import Plugin, Query, Settings, Option
 
-class ReturnListPlugin(Plugin):
+class YieldOptionsPlugin(Plugin):
     async def __call__(self, data: Query, settings: Settings):
         yield Option(f"Your text is: {data.text}")
         yield Option(f"Your keyword is: {data.keyword}")
         yield Option(f"Your raw text is: {data.raw_text}", sub="keyword + text")
 
 if __name__ == "__main__":
-    ReturnListPlugin().run()
+    YieldOptionsPlugin().run()
