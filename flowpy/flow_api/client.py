a8f788a4bac303a82193e944b9742037f7bcd26a
cibere <71997063+cibere@users.noreply.github.com>
2024-11-13 09:27:24 -0800

    make changes to flow api

--- flowpy/flow_api/client.py
+++ flowpy/flow_api/client.py
@@ -2,6 +2,8 @@ from ..jsonrpc import JsonRPCClient, Result
 
 from .fuzzy_search import FuzzySearchResult
 
+__all__ = "FlowLauncherAPI",
+
 class FlowLauncherAPI:
     def __init__(self, jsonrpc: JsonRPCClient):
         self.jsonrpc = jsonrpc
@@ -20,14 +22,14 @@ class FlowLauncherAPI:
         assert isinstance(res, Result)
         return res
     
-    async def show_error_message(self, title: str, subtitle: str) -> None:
-        res = await self.jsonrpc.request("ShowMsgError", [title, subtitle])
+    async def show_error_message(self, title: str, text: str) -> None:
+        res = await self.jsonrpc.request("ShowMsgError", [title, text])
         assert isinstance(res, Result)
     
-    """async def show_message(self, title: str, subtitle: str = "", icon: str = "") -> Result:
-        res = await self.jsonrpc.request("ShowMsgError", [title, subtitle, icon])
+    async def show_message(self, title: str, text: str, icon: str = "", use_main_window_as_owner: bool = True) -> Result:
+        res = await self.jsonrpc.request("ShowMsg", [title, text, icon, use_main_window_as_owner])
         assert isinstance(res, Result)
-        return res"""
+        return res
     
     async def open_settings_menu(self) -> None:
         res = await self.jsonrpc.request("OpenSettingDialog")
