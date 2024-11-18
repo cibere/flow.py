365ca08f4f329384e7a4f471fd7a558dc9e956b6
cibere <71997063+cibere@users.noreply.github.com>
2024-11-17 22:00:26 -0800

    Update fuzzy_search.py

--- flowpy/flow_api/fuzzy_search.py
+++ flowpy/flow_api/fuzzy_search.py
@@ -4,6 +4,21 @@ __all__ = ("FuzzySearchResult",)
 
 
 class FuzzySearchResult(Base):
+    r"""A class which represents the result given from flow launcher to a fuzzy search request
+
+    .. NOTE::
+        This is not intended to be a class that you create yourself, use :func:`FlowLauncherAPI.fuzzy_search` instead.
+    
+    Attributes
+    --------
+    score: :class:`int`
+        The score of the result
+    match_data: list[:class:`int`]
+        The match data included in the result
+    search_percision: :class:`int`
+        The perision of the result
+    """
+
     score: int = add_prop("score")
     match_data: list[int] = add_prop("matchData")
     search_precision: int = add_prop("searchPrecision")
