83d254b32c086b3f374c15eec8b9ac8cab96f1b9
cibere <71997063+cibere@users.noreply.github.com>
2024-11-17 18:06:48 -0800

    Update errors.py

--- flowpy/jsonrpc/errors.py
+++ flowpy/jsonrpc/errors.py
@@ -1,10 +1,22 @@
 __all__ = ("JsonRPCException", "JsonRPCVersionMismatch")
 
 
-class JsonRPCException(Exception): ...
+class JsonRPCException(Exception):
+    r"""This is a base class which represents errors with the jsonrpc client
+    """
 
 
 class JsonRPCVersionMismatch(JsonRPCException):
+    r"""This is raised when the jsonrpc client receives a jsonrpc version which it isn't looking for
+    
+    Attributes
+    --------
+    expected: :class:`str`
+        The Json RPC version it expected
+    received: :class:`str`
+        The Json RPC version it received
+    """
+    
     def __init__(self, expected: str, received: str) -> None:
         super().__init__(
             f"Expected to get version {expected}, but got {received} instead."
