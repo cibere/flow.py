be15ddafb46613aa218f6a277a37bf3f8db2c8b6
cibere <71997063+cibere@users.noreply.github.com>
2024-11-17 22:00:22 -0800

    Create SettingsTemplate.yaml

--- flowpy/flow_api/plugin_metadata.py
+++ flowpy/flow_api/plugin_metadata.py
@@ -12,6 +12,33 @@ __all__ = ("PluginMetadata",)
 
 
 class PluginMetadata(Base):
+    r"""This class represents a plugin's metadata
+    
+    Attributes
+    --------
+    id: :class:`str`
+        The plugin's ID
+    name: :class:`str`
+        The plugin's name
+    author: :class:`str`
+        The name of the plugin's author
+    version: :class:`str`
+        The current version of the plugin
+    language: :class:`str`
+        The language that the plugin is written in. Possible values: "csharp", "executable", "fsharp", "python", "javascript", "typescript", "python_v2", "executable_v2", "javascript_v2", "typescript_v2".
+    description: :class:`str`
+        The plugin's description
+    website: :class:`str`
+        A link to the plugin's website
+    disabled: :class:`bool`
+        Whether the plugin is disabled or not
+    directory: :class:`str`
+        The path to the plugin's directory
+    keywords: list[:class:`str`]
+        A list of the plugin's keywords
+    main_keyword: :class:`str`
+        The plugin's main keyword
+    """
     def __init__(self, data: dict[str, Any], flow_api: FlowLauncherAPI) -> None:
         super().__init__(data)
         self._flow_api = flow_api
@@ -41,26 +68,40 @@ class PluginMetadata(Base):
 
     @property
     def executable(self) -> Path:
+        r""":class:`pathlib.Path`: The path to the plugin's executable file"""
         return Path(self._data["executeFilePath"]).absolute()
 
     @property
     def icon(self) -> Path:
+        r""":class:`pathlib.Path`: The path to the plugin's icon file"""
         return Path(self._data["icoPath"]).absolute()
 
     def add_keyword(self, keyword: str) -> Awaitable[None]:
-        """
+        r"""|coro|
+        
         Registers a new keyword with flow for the plugin.
 
-        This is a shortcut to `flowpy.flow_api.FlowLauncherAPI.add_keyword`
+        This is a shortcut to :func:`FlowLauncherAPI.add_keyword`
+
+        Parameters
+        --------
+        keyword: :class:`str`
+            The keyword to be added
         """
 
         return self._flow_api.add_keyword(self.id, keyword)
 
     def remove_keyword(self, keyword: str) -> Awaitable[None]:
-        """
+        """|coro|
+
         Removes a keyword from the plugin.
 
-        This is a shortcut to `flowpy.flow_api.FlowLauncherAPI.remove_keyword`
+        This is a shortcut to :func:`FlowLauncherAPI.remove_keyword`
+
+        Parameters
+        --------
+        keyword: :class:`str`
+            The keyword to be removed
         """
 
         return self._flow_api.remove_keyword(self.id, keyword)
