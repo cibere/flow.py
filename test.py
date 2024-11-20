d7a76d67e95f5b076df06599852cbd7501f9808f
cibere <71997063+cibere@users.noreply.github.com>
2024-11-19 19:45:42 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,19 @@
-import logging
+from flowpy import Option, Plugin, Query
 
-from flowpy import Action, ExecuteResponse, Option, Plugin, Query
-
-log = logging.getLogger("Test Plugin")
 plugin = Plugin()
-
-
-async def my_action(text: str):
-    await plugin.api.show_notification("Test Plugin", f"Your text is: {text}")
-    return ExecuteResponse(hide=False)
 
 
 @plugin.event
 async def on_query(data: Query):
-    yield Option(
-        f"Your text is: {data.text}",
-        sub="Click this to initiate the action",
-        action=Action(my_action, data.text),
-    )
+    result = await plugin.api.fuzzy_search(data.text, "Flow")
+    return [
+        Option(
+            f"Flow: {result.score}",
+            sub=f"Precision: {result.search_precision}",
+            title_highlight_data=result.highlight_data,
+        )
+
+    ]
 
 
-@plugin.event
-async def on_initialization():
-    log.info("Plugin has been initialized")
-
-
-if __name__ == "__main__":
-    plugin.run()
+plugin.run()
