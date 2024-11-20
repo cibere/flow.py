0d8fd63a84d9fb4c6cf2b9c16777614b1178d27b
cibere <71997063+cibere@users.noreply.github.com>
2024-11-19 17:27:44 -0800

    add MISSING.__repr__

--- flowpy/utils.py
+++ flowpy/utils.py
@@ -10,6 +10,9 @@ class _MissingSentinel:
 
     def __eq__(self, other: Any) -> bool:
         return False
+    
+    def __repr__(self) -> str:
+        return "..."
 
 
 MISSING: Any = _MissingSentinel()
