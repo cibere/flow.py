cf974d9edeb90faa632807f7df6706975375dbe6
cibere <71997063+cibere@users.noreply.github.com>
2024-12-04 08:58:48 -0800

    bump version to dev version

--- flogin/__init__.py
+++ flogin/__init__.py
@@ -1,6 +1,6 @@
 __title__ = "flogin"
 __author__ = "cibere"
-__version__ = "0.0.5"
+__version__ = "0.0.6a"
 
 
 from typing import Literal, NamedTuple
@@ -21,6 +21,6 @@ class VersionInfo(NamedTuple):
     releaselevel: Literal["alpha", "beta", "candidate", "final"]
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=5, releaselevel="final")
+version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=6, releaselevel="alpha")
 
 del NamedTuple, Literal, VersionInfo
