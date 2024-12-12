dd5f433edd0118cfe284ce8bec4768f592003192
cibere <71997063+cibere@users.noreply.github.com>
2024-12-11 21:05:07 -0800

    update JsonRPCClient to hopefully fix high CPU useage in certain cases

--- flogin/jsonrpc/client.py
+++ flogin/jsonrpc/client.py
@@ -60,7 +60,7 @@ class JsonRPCClient:
                 f"Failed to cancel task with id of {id!r}, could not find task."
             )
 
-    async def __handle_result(self, result: dict) -> None:
+    async def handle_result(self, result: dict) -> None:
         rid = result["id"]
 
         LOG.debug(f"Result: {rid}, {result!r}")
@@ -74,13 +74,13 @@ class JsonRPCClient:
                 f"Result from unknown request given. ID: {rid!r}, result={result!r}"
             )
 
-    async def __handle_error(self, id: int, error: ErrorResponse) -> None:
+    async def handle_error(self, id: int, error: ErrorResponse) -> None:
         if id in self.requests:
             self.requests.pop(id).set_exception(Exception(error))
         else:
             LOG.error(f"cancel with no id found: %d", id)
 
-    async def __handle_notification(self, method: str, params: dict[str, Any]) -> None:
+    async def handle_notification(self, method: str, params: dict[str, Any]) -> None:
         if method == "$/cancelRequest":
             await self.__handle_cancellation(params["id"])
         else:
@@ -89,7 +89,7 @@ class JsonRPCClient:
                 exc_info=JsonRPCException("Unknown notificaton method received"),
             )
 
-    async def __handle_request(self, request: dict[str, Any]) -> None:
+    async def handle_request(self, request: dict[str, Any]) -> None:
         method: str = request["method"]
         params: list[Any] = request["params"]
         task = None
@@ -123,43 +123,44 @@ class JsonRPCClient:
                 ErrorResponse.internal_error().to_message(id=request["id"])
             )
 
+    async def process_input(self, line: str):
+        LOG.debug(f"Processing {line!r}")
+        message = json.loads(line)
+
+        if "id" not in message:
+            LOG.debug(f"Received notification: {message!r}")
+            await self.handle_notification(message["method"], message["params"])
+        elif "method" in message:
+            LOG.debug(f"Received request: {message!r}")
+            await self.handle_request(message)
+        elif "result" in message:
+            LOG.debug(f"Received result: {message!r}")
+            await self.handle_result(message)
+        elif "error" in message:
+            LOG.exception(f"Received error: {message!r}")
+            await self.handle_error(
+                message["id"], ErrorResponse.from_dict(message["error"])
+            )
+        else:
+            LOG.exception(
+                f"Unknown message type received",
+                exc_info=JsonRPCException("Unknown message type received"),
+            )
+
     async def start_listening(self, reader: StreamReader, writer: StreamWriter):
         self.reader = reader
         self.writer = writer
 
         while 1:
-            bytes = await reader.readline()
-            line = bytes.decode("utf-8")
-            if line == "":
-                continue
+            async for line in reader:
+                LOG.debug(f"Received line: {line!r} of type {type(line)!r}")
+                line = line.decode("utf-8")
+                if line == "":
+                    continue
 
-            LOG.info(f"Received line: {line}")
-            message = json.loads(line)
+                asyncio.create_task(self.process_input(line))
 
-            if "id" not in message:
-                LOG.debug(f"Received notification: {message!r}")
-                asyncio.create_task(
-                    self.__handle_notification(message["method"], message["params"])
-                )
-            elif "method" in message:
-                LOG.debug(f"Received request: {message!r}")
-                asyncio.create_task(self.__handle_request(message))
-            elif "result" in message:
-                LOG.debug(f"Received result: {message!r}")
-                asyncio.create_task(self.__handle_result(message))
-            elif "error" in message:
-                LOG.exception(f"Received error: {message!r}")
-                asyncio.create_task(
-                    self.__handle_error(
-                        message["id"], ErrorResponse.from_dict(message["error"])
-                    )
-                )
-            else:
-                LOG.exception(
-                    f"Unknown message type received",
-                    exc_info=JsonRPCException("Unknown message type received"),
-                )
-
+            
     async def write(self, msg: bytes, drain: bool = True) -> None:
         LOG.debug(f"Sending: {msg!r}")
         self.writer.write(msg)
