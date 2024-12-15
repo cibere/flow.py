d0787fd1e1fe58b8d9829cd6393747bfa802626a
cibere <71997063+cibere@users.noreply.github.com>
2024-12-15 14:07:47 -0800

    remove SettingNotFound error

--- flogin/errors.py
+++ flogin/errors.py
@@ -2,28 +2,12 @@ from __future__ import annotations
 
 __all__ = (
     "PluginException",
-    "SettingNotFound",
     "PluginNotInitialized",
 )
 
 
 class PluginException(Exception):
     r"""A class that represents exceptions with your plugin"""
-
-
-class SettingNotFound(PluginException):
-    r"""This gets raised when you try to access a setting key that doesn't exist
-
-    Attributes
-    -----------
-    name: :class:`str`
-        The name of the setting key
-    """
-
-    def __init__(self, name: str) -> None:
-        super().__init__(f"Setting {name!r} was not found.")
-        self.name = name
-
 
 class PluginNotInitialized(PluginException):
     r"""This is raised when you try to access something that needs data from the initialize method, and it hasn't been called yet."""
