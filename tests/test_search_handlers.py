4e007699cbb254fdb3b30198b25b3c3eb24a7f42
cibere <71997063+cibere@users.noreply.github.com>
2024-12-18 19:46:49 -0800

    add test for SearchHandler.on_error

--- tests/test_search_handlers.py
+++ tests/test_search_handlers.py
@@ -59,7 +59,7 @@ handlers = [
 ]
 
 
-@pytest.fixture(params=handlers, ids=lambda h: h.__class__.__name__, autouse=True)
+@pytest.fixture(params=handlers, ids=lambda h: h.__class__.__name__)
 def handler(plugin: Plugin, request: pytest.FixtureRequest):
     h = request.param
     plugin.register_search_handler(h)
@@ -67,7 +67,20 @@ def handler(plugin: Plugin, request: pytest.FixtureRequest):
 
 
 @pytest.mark.asyncio
-async def test_handler_result(tester: PluginTester):
+async def test_handler_result(tester: PluginTester, handler: SearchHandler):
     response = await tester.test_query("bar", keyword="foo")
     result = response.results[0]
     assert result.title == "Title"
+
+@pytest.mark.asyncio
+async def test_handler_error(plugin: Plugin, tester: PluginTester):
+    @plugin.search()
+    async def handler(query: Query):
+        raise TypeError("Boo")
+    
+    @handler.error
+    async def error_handler(query: Query, error: Exception):
+        assert isinstance(error, TypeError)
+        assert str(error) == "Boo"
+    
+    await tester.test_query("bar")
